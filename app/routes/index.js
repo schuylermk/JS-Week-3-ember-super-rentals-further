b59b51975309410f5e520d8e9c4da4f9420202f5
chris berry and schuyler klaassen <Guest@epicodus-2.local>
2016-03-30 11:55:50 -0700

    trying to add new cities

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return Ember.RSVP.hash ({
+      rentals: this.store.findAll('rental'),
+      cities: this.store.findAll('city')
+    });
   },
   actions: {
     save3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
+      this.transitionTo('index');
+    },
+
+    save6(params) {
+      var newCity =this.store.createRecord('city', params);
+      newCity.save();
       this.transitionTo('index');
     },
 
